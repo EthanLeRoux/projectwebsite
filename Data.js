cd96af81068d0ecf64fcc00717a271979b4a0cbe
xxNecromancerx <49578340+xxNecromancerx@users.noreply.github.com>
2023-10-23 18:26:44 +0100

    Revert "data_update(nathan)"
    This reverts commit c4774604912c5335fe93daffd7ef0fea03f0a1f5.

--- Data.js
+++ Data.js
@@ -1,128 +1,47 @@
 //data for the main page, gets called in the index and cart.js/html
 let shopItemsData =[
     {
-        id: "frh",
-        img:"image/Airin_G-1_Black_&_White_Leather_Bomber_Jacket.jpg",
-        name: "Airin G-1 Black & White Leather Bomber Jacket",
-        price: 2600,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "df",
-        img:"image/Bomber-Brown-Leather-Jacket-With-Hood-3.jpg",
-        name: "Bomber Brown Leather Jacket With Hood",
-        price: 2100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "fddf",
-        img:"image/Bowler_Style_Leather_Shoulder_Bag.jpeg",
-        name: "Bowler Style Leather Shoulder Bag",
-        price: 2600,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "ff",
-        img:"image/Ionic_Black_Leather_Jacket.jpeg",
-        name: "Ionic Black Leather Jacket",
-        price: 2100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "ghj",
-        img:"image/Lavendard_Brown_Leather_Biker_Jacket.jpeg",
-        name: "Lavender Brown Leather Biker Jacket",
-        price: 2100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
         id: "osgsesgsne",
-        img:"image/mens-leather-jacket-starlord-2_2.jpg",
-        name: "Mens Leather Jacket Starlord",
-        price: 2100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "lll",
-        img:"image/NeverBasic_RESHOOT_crop_center.jpg",
-        name: "Women's Rumy Black Leather Biker Jacket",
-        price: 2600,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "wowj",
-        img:"image/Plus_Size_Fox_Leather_Jacket.png",
-        name: "Women's Fox Leather Jacket",
-        price: 2600,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "dhuhnk",
-        img:"image/Vaxton_Brown_Hybrid_Varsity_Jacket.jpg",
-        name: "Vaxton Brown Hybrid Varsity Jacket",
-        price: 2600,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-
-    
-    },
-    {
-        id: "nldnka",
-        img:"image/Vintage_Leather_Handbag_Cross_Body_Shoulder_Bag_With_Rivet.jpeg",
-        name: "Vintage Leather Handbag Cross Body Shoulder Bag With Rivet",
-        price: 2600,
+        img:"images/L6.png",
+        name: "bag",
+        price: 25,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
 
     
     },
     
     {
-        id:  "wiokdl",
-        img:"image/Women_Lamarque_Donna_Black_Leather_Jacket.png",
-        name: "Women's Lamarque Donna Black Leather Jacket",
-        price: 2600,
+        id:  "gsegesgse",
+        img:"images/L6.png",
+        name: "bag13",
+        price: 25,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
 
     },
     
     {
         id:  "segesgseg",
-        img:"image/Womens_Rumy_Maroon_Leather_Biker_Jacket.jpg",
-        name: "Womens Rumy Maroon Leather Biker Jacket",
-        price: 2600,
+        img:"images/L6.png",
+        name: "bdag13",
+        price: 25,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
 
     },
     
     {
         id:  "jioanf",
-        img:"image/Womens_Tomachi_Red_Leather_Jacket.jpeg",
-        name: "Womens Tomachi Red Leather Jacket",
-        price: 2600,
+        img:"images/L6.png",
+        name: "bags13",
+        price: 25,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
 
     },
     
     {
         id:  "biuabfseb",
-        img:"image/Womens_Vixen_Brown_Classic_Collar_Leather_Jacket.jpg",
-        name: "Womens Vixen Brown Classic Collar Leather Jacket",
-        price: 2600,
+        img:"images/L6.png",
+        name: "bag155",
+        price: 25,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
 
 
